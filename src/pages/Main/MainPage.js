8c277094268fb3d3b9a0b1fb4df9b78963a8cc02
Cho SangHyun <98shcho@naver.com>
2023-04-27 18:11:53 +0900

    feat: 메인에서 로그인페이지로 가는 기능 추가
    로그인 안 했으면 메인페이지에서 로그인 페이지로 가도록 기능을 추가함

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Common/Header';
 import Footer from '../../components/Common/Footer';
 import Board from '../../components/Board/Board';
 import Register from '../../components/Register/Register';
+import { useRecoilValue } from 'recoil';
+import { currentUserNameState } from '../../atoms';
+import { useNavigate } from 'react-router';
 
 const MainPage = (props) => {
+    const currentUserName = useRecoilValue(currentUserNameState);
+    const navigate = useNavigate()
 
+    useEffect(() => {
+        if (!currentUserName) {
+            navigate("/login");
+        }
+    }, [currentUserName])
 
     return(
         <div>
