520199002b50bdfa4958f4117d4748dc34eaf735
Cho SangHyun <98shcho@naver.com>
2023-04-27 19:19:10 +0900

    feat: 게시된 커피챗에 참가하는 기능 구현

--- src/components/Board/BoardEle.js
+++ src/components/Board/BoardEle.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './BoardEle.css'
+import { useRecoilValue } from 'recoil';
+import { currentUserNameState } from '../../atoms';
 
 // props : no
 // => title, time, type, curNum, totNum, owner, state 
 const BoardEle = ({id, title, revervation_date, category, writer, registrant_count, current_count}) => {
+    const currentUserName = useRecoilValue(currentUserNameState);
     const [text, setText] = useState(['NO.', '제목', '날짜 및 시간', '카테고리', '접수 인원', '작성자', '등록']);
     const [classText, setClassText] = useState('ElementBoxBold');
+
+    const join = (e) => {
+        if (e.target.classList.contains("join") && window.confirm(`[${title}]을(를) 신청하시겠습니까?`)) {
+            fetch(`http://localhost:8080/board/register/${id}/${currentUserName}`)
+            .then(res => res.json())
+            .then(res => console.log(res));
+        }
+    }
+
     useEffect(() => {
         if (id !== 0) {
             setClassText('ElementBox');
 
-            // api로 부터 값들을 받아와서 text list를
-            // 변경해야됨. props.no로 부터 받아온 값들을
-            // text에 적용해야됨.
-
-            setText([id, title, revervation_date, category, `${current_count}/${registrant_count}`, writer, '등록'])
+            setText([id, title, revervation_date, category, 
+                `${current_count}/${registrant_count}`, writer,
+                currentUserName === writer ? "방장" : (registrant_count === current_count ? "마감" : "등록")])
         }
     }, []);
 
@@ -26,7 +36,10 @@ const BoardEle = ({id, title, revervation_date, category, writer, registrant_cou
             <div className='typeOp'>{text[3]}</div>
             <div className='curNumOp'>{text[4]}</div>
             <div className='ownerOp'>{text[5]}</div>
-            <div className='stateOp'>{text[6]}</div>
+            <div 
+                onClick={join}
+                className={`${id === 0 ? 'first' : ''} stateOp ${currentUserName === writer ? "leader" : (registrant_count === current_count ? "done" : "join")}`}
+            >{id === 0 ? text[6] : `[ ${text[6]} ]`}</div>
         </div>
     )
 }
